929e384dcbbfab4a62d5da25455c2ababfcc394d
Aneli64 <eliandeniz27@gmail.com>
2026-09-16 14:54:58 +0200

    code of proyect finish

--- Coffe_Machine_Proyect/coffeMachine.py
+++ Coffe_Machine_Proyect/coffeMachine.py
@@ -1,35 +1,63 @@
 from Coffe_Machine_Proyect.coffee import CoffeeMenu
 
+
 class CoffeMachine:
     def __init__(self, water, milk, coffe, money):
         self.water = water
         self.milk = milk
         self.coffe = coffe
         self.money = money
-        self.machine_ingredients = [self.water, self.milk, self.coffe, self.money]
+        self.machine_ingredients = [self.water, self.milk, self.coffe]
 
     def coffe_info(self) -> str:
-        return f"Water : {self.water}\n Milk : {self.milk}\n Coffee : {self.coffe}\n Money : {self.money}\n"
+        return f"Water : {self.water}\n Milk : {self.milk}\n Coffee : {self.coffe}\n"
+
+    def available_coffe(self, coffee_type: str) -> bool:
+        bool_coffee = False
+        match coffee_type:
+            case "espresso":
+                coffee = CoffeeMenu.espresso
+            case "latte":
+                coffee = CoffeeMenu.latte
+            case "cappuccino":
+                coffee = CoffeeMenu.cappuccino
+            case "off":
+                bool_coffee = False
+                exit()
+            case _:
+                bool_coffee = False
+                print("Invalid coffee type")
+                return False
+
+        for machine_ing, (ing_name, coffee_ing) in zip(self.machine_ingredients, coffee.items()):
+            if machine_ing < coffee_ing:
+                print(f"Sorry, there is not enough {ing_name}")
+                return False
+            else:
+                return True
 
     def choose_coffe_type(self):
         coffe_input = input("What would you like? (espresso/latte/cappuccino): ")
-        match coffe_input:
-            case "espresso":
-                cof_bool = False
-                for machin_ing, (ing_name, espresso_ing) in zip(self.machine_ingredients, CoffeeMenu.espresso.items()):
-                    if machin_ing < espresso_ing:
-                        print(f"Sorry there is not enough {ing_name}")
-                        cof_bool = False
-                        break
-                    else:
-                        cof_bool = True
-                if cof_bool == True:
-                    print("Here is your espresso. Enjoy!”")
-            case "latte":
-                print("latte option")
-            case "espresso":
-                print("cappuccino option")
-            case "report":
-                self.coffe_info()
-            case "off":
-                exit()
+        coins_in_machine = 0.0
+        cost_of_coffee = 0.0
+
+        if self.available_coffe(coffe_input):
+            coins_input = float(input("Insert coins: "))
+            match coffe_input:
+                case "espresso":
+                    cost_of_coffee = CoffeeMenu.espresso["cost"]
+                case "latte":
+                    cost_of_coffee = CoffeeMenu.latte["cost"]
+                case "cappuccino":
+                    cost_of_coffee = CoffeeMenu.cappuccino["cost"]
+                case _:
+                    exit()
+            while coins_in_machine < cost_of_coffee:
+                coins_in_machine += coins_input
+                print(f"COINS IN MACHINE: {coins_in_machine}")
+                if coins_in_machine >= cost_of_coffee:
+                    change = coins_in_machine - cost_of_coffee
+                    break
+                coins_input = float(input("Insert coins: "))
+
+            print(f"Enjoy your coffee!!, your change is: {change}")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Coffe_Machine_Proyect.coffeMachine import CoffeMachine
 
-def main():
-    # TODO: CREATE A COFFEE MACHINE
-    coffeMachine = CoffeMachine(100, 50, 76, 2.5)
 
+def main():
+    # TODO: CREATE COFFEE MACHINE AND FILL INGREDIENTS
+    coffeMachine = CoffeMachine(100, 50, 76, 2.5)
     # TODO: PRINT A REPORT
     print(coffeMachine.coffe_info())
+    # TODO: CHOOSE A COFFEE
     coffeMachine.choose_coffe_type()
 
 
